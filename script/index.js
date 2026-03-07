61a9242078db23844219704ec2ec4f1f0e50c00e
shahin200294 <shahinsorker0483@gmail.com>
2026-03-07 19:16:14 +0600

    Add client-side validation to login form for username and password ,Fix typo in password validation logic

--- script/index.js
+++ script/index.js
@@ -1,7 +1,17 @@
+
 function login(){
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     
-    const errorMsg =
+    const errorMsg =document.getElementById("errorMsg");
+
+    if(username === "admin" && password === "admin123"){
+        alert("Login successful!");
+            window.location.href = "dashboard.html";
+    }else{
+        errorMsg.classList.remove("hidden");
+        console.log("Invalid username or password");
+    }
+    
     
 }
